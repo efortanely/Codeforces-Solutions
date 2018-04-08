8296ad74cf0f3efac16ce5cf48c90468e341b812
roseequalssinfourtheta <rosemary.fortanely@gmail.com>
2018-04-07 19:20:42 -0500

    Update p405a.java

--- p405a.java
+++ p405a.java
@@ -1,27 +1,17 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class p405a {
-	public static void main(String[] s) {
+public class GravityFlip {
+	public static void main(String[] s){
 		Scanner sc = new Scanner(System.in);
 		int numCol = sc.nextInt();
 		int[] input = new int[numCol];
-
-		for (int i = 0; i < numCol; i++) {
+		
+		for(int i = 0; i < numCol; i++)
 			input[i] = sc.nextInt();
-		}
-
-		for (int i = 0; i < input.length; i++) {
-			for (int j = 1; j < input.length - i; j++) {
-				if (input[j - 1] > input[j]) {
-					int temp = input[j - 1];
-					input[j - 1] = input[j];
-					input[j] = temp;
-				}
-			}
-		}
-
-		for (int i = 0; i < input.length; i++) {
+		
+		Arrays.sort(input);
+		
+		for(int i = 0; i < input.length; i++)
 			System.out.print(input[i] + " ");
-		}
 	}
 }
